a179ff1a56cfe45cf3d2244cb4755aa04406c568
Nikhil Savant <123323767+nsavant12@users.noreply.github.com>
2023-09-15 20:24:31 -0500

    Update Skills.tsx

--- pages/Skills.tsx
+++ pages/Skills.tsx
@@ -4,6 +4,16 @@ import Skill from "./Skill";
 type Props = {}
 
 function Skills({}: Props) {
+  const skillsData = [
+    { name: 'HTML', proficiency: '90%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/html-1.svg' },
+    { name: 'CSS', proficiency: '95%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/css-3.svg' },
+    { name: 'Python', proficiency: '80%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/python-5.svg' },
+    { name: 'Java', proficiency: '70%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/java-14.svg' },
+    { name: 'JavaScript', proficiency: '60%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/logo-javascript.svg' },
+    { name: 'TypeScript', proficiency: '55%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/typescript.svg' },
+    { name: 'Next.js', proficiency: '75%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/next-js.svg' },
+    { name: 'React', proficiency: '75%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/react-2.svg' },
+  ];
   return (
     
     <motion.div 
@@ -14,18 +24,14 @@ function Skills({}: Props) {
         <h3 className='tracking-[3px] uppercase text-xl top-36 pt-10'>Hover over a skill for current proficiency</h3>
         <div className='flex pt-10'>
         <div className='grid grid-cols-4 gap-5 mx-auto'>
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
-            <Skill />
+          {skillsData.map((skill, index) => (
+            <Skill
+              key={index}
+              iconUrl={skill.iconUrl}
+              proficiency={skill.proficiency}
+              directionLeft={index % 2 !== 0}
+            />
+          ))}
         </div>
         </div>
     </motion.div>
