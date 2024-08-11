95f1279ba10926cb9a4090326bc6f49a7b40870b
Nikhil Savant <123323767+nsavant12@users.noreply.github.com>
2024-08-11 13:23:13 -0500

    Update Skills.tsx

--- pages/Skills.tsx
+++ pages/Skills.tsx
@@ -7,12 +7,12 @@ function Skills({}: Props) {
   const skillsData = [
     { name: 'HTML', proficiency: '90%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/html-1.svg' },
     { name: 'CSS', proficiency: '95%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/css-3.svg' },
-    { name: 'Python', proficiency: '80%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/python-5.svg' },
+    { name: 'Python', proficiency: '65%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/python-5.svg' },
     { name: 'Java', proficiency: '70%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/java-14.svg' },
-    { name: 'JavaScript', proficiency: '60%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/logo-javascript.svg' },
-    { name: 'TypeScript', proficiency: '55%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/typescript.svg' },
-    { name: 'Next.js', proficiency: '75%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/next-js.svg' },
-    { name: 'React', proficiency: '75%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/react-2.svg' },
+    { name: 'JavaScript', proficiency: '50%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/logo-javascript.svg' },
+    { name: 'TypeScript', proficiency: '50%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/typescript.svg' },
+    { name: 'Next.js', proficiency: '60%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/next-js.svg' },
+    { name: 'React', proficiency: '65%', iconUrl: 'https://cdn.worldvectorlogo.com/logos/react-2.svg' },
   ];
   return (
     
